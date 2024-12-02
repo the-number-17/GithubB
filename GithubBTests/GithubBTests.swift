a4e5009a89258353b25c2454a9568b58ea3a8bd7
Aviral Singh <149692321+the-number-17@users.noreply.github.com>
2024-12-02 15:36:52 +0530

    Initial Commit

---
+++ GithubBTests/GithubBTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubBTests.swift
+//  GithubBTests
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import Testing
+@testable import GithubB
+
+struct GithubBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
